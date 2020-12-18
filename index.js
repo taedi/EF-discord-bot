59d929915f8c0874e08031d21b22116c964940f6
seomingyu11 <seomingyu11@gmail.com>
2020-12-18 17:43:08 +0900

    Add comment

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * ' // ' 은 주석(프로그램 실행 시 영향 X)입니다.
+ * 한국어 앞에 있는 ' // ' 을 삭제할 시 프로그램 실행할 때 오류가 발생하게 됩니다
+ * 영어 앞에 있는 ' // ' 만 삭제해 주세요!
+ */
+
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
@@ -14,16 +20,26 @@ const updateMembers = () => {
   }
 
   // client.channels.cache.get(settings.statsChannel.all).setName('All: ' + stats.all)
+  // 전체 멤버 수를 디스코드 서버 채널에 적용
+
   // client.channels.cache.get(settings.statsChannel.member).setName('Member: ' + stats.member)
+  // 유저 수를 디스코드 서버 채널에 적용
+
   // client.channels.cache.get(settings.statsChannel.bot).setName('Bot: ' + stats.bot)
+  // 봇 수를 디스코드 서버 채널에 적용
+
   client.channels.cache.get(settings.statsChannel.voice).setName('📡ㅣ음성 연결 : ' + stats.voice)
-  console.log("Voice connection updated", stats.voice)
+  // 음성 채널에 접속한 유저 수를 서버 채널에 적용
   
   return console.log("Member count updated", stats)
 }
 
 // client.on('guildMemberJoin', () => updateMembers)
+// 서버에 멤버가 들어왔을 때 실행
+
 // client.on('guildMemberRemove', () => updateMembers)
+// 서버에서 멤버가 나갔을 떄 실행
+
 client.on('voiceStateUpdate', (oldState, newState) => {
   const voiceChannels = newState.member.guild.channels.cache.filter(c => c.type === 'voice')
   let count = 0
